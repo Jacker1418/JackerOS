fc9614f082864f07be0790ef7ea7f6417b8fd4f9
Jacker1418 <jacker1418@gmail.com>
2019-08-01 10:48:38 -0700

    Chapter 8 Check Memory Space & Control A20 Gate for accessing entire memory

--- Kernel32/Source/Main.c
+++ Kernel32/Source/Main.c
@@ -1,13 +1,35 @@
 #include "Types.h"
 
 void printString(int in_x, int in_y, const char* in_string);
+BOOL Initialize_Kernel64_Area(void);
+BOOL Is_Memory_Enough(void);
 
 void Main(void){
 
-	printString(0, 3, "First C Source Code in Kernel32");
+	int numLine = 3;
 
-	while(1);
+	printString(0, numLine++, "First C Source Code in Kernel32 [PASS]");
 
+	printString(0, numLine, "Minimum Memory size Check...................[    ]");
+	if(Is_Memory_Enough()){
+		printString(45, numLine, "Pass");
+	}else{
+		printString(45, numLine, "Fail");
+		printString(0, ++numLine, "Not Enough Memory, JackerOS Requires Over 64M byte Memory");
+
+		while(1);
+	}
+	numLine++;
+
+	printString(0, numLine, "IA-32e Kernel Area Initialize...............[    ]" );
+	if(Initialize_Kernel64_Area() ){
+		printString(45, numLine, "Pass");
+	}else{
+		printString(45, numLine, "Fail");
+		printString(0, numLine++, "Kernel Area Initialize Fail");
+
+		while(1);
+	}
 }
 
 void printString(int in_x, int in_y, const char* in_string){
@@ -20,4 +42,39 @@ void printString(int in_x, int in_y, const char* in_string){
 	for(i = 0; in_string[i] != 0; i++){
 		pScreen[i].bCharacter = in_string[i];
 	}
+}
+
+BOOL Initialize_Kernel64_Area(void){
+	DWORD* currentAddress;
+
+	currentAddress = (DWORD *)0x100000;
+
+	while( (DWORD) currentAddress < 0x600000){
+		*currentAddress = 0x00;
+
+		if( *currentAddress != 0x00){
+			return FALSE;
+		}
+
+		currentAddress++;
+	}
+
+	return TRUE;
+}
+
+BOOL Is_Memory_Enough(void){
+	DWORD *currentAddress;
+
+	currentAddress = (DWORD *) 0x100000;
+
+	while( (DWORD) currentAddress < 0x4000000){
+		*currentAddress = 0x12345678;
+
+		if(*currentAddress != 0x12345678){
+			return FALSE;
+		}
+
+		currentAddress += (0x100000 / 4);
+	}
+	return TRUE;
 }
